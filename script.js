e788255e055d55799811c8f3455d68944d3744a4
Sayed-Oni <onihassan44@gmail.com>
2021-12-19 06:01:09 +0600

    DEMO DONE

--- script.js
+++ script.js
@@ -7,24 +7,6 @@ document.querySelector('#search-btn').onclick = () =>{
     navbar.classList.remove('active');
 }
 
-let cart = document.querySelector('.shopping-cart-container');
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cart.classList.toggle('active');
-    searchForm.classList.remove('active');
-    loginForm.classList.remove('active');
-    navbar.classList.remove('active');
-}
-
-let loginForm = document.querySelector('.login-form-container');
-
-document.querySelector('#login-btn').onclick = () =>{
-    loginForm.classList.toggle('active');
-    searchForm.classList.remove('active');
-    cart.classList.remove('active');    
-    navbar.classList.remove('active');
-}
-
 let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-btn').onclick = () =>{
@@ -37,16 +19,3 @@ document.querySelector('#menu-btn').onclick = () =>{
 window.onscroll = () =>{
     navbar.classList.remove('active');
 }
-
-document.querySelector('.home').onmousemove = (e) =>{
-
-    let x = (window.innerWidth - e.pageX * 2) / 90;
-    let y = (window.innerHeight - e.pageY * 2) / 90;
-
-    document.querySelector('.home .home-parallax-img').style.transform = `translateX(${y}px) translateY(${x}px)`;
-}
-
-document.querySelector('.home').onmouseleave = () =>{
-
-    document.querySelector('.home .home-parallax-img').style.transform = `translateX(0px) translateY(0px)`;
-}   
